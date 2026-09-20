e7db48ce49fc04dbc3306b90bee97a09be23106f
Sakshi Kokate <kokatesakshi0@gmail.com>
2026-09-20 08:55:24 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr [5]= {3,4,5,6,7};
+    cout<<arr[0]<< " "<< arr[1]<< " "<< arr[2]<< " "<<arr[3]<< " "<<arr[4];
+
+    cout << "Frist number is \n", arr[0];
+
+    return 0;
+}
